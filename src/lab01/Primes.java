1d7e8ed6d5a823c9a25908f9456a430a660135ce
Stepan <stepan.de@mail.ru>
2021-02-14 19:56:08 +0300

    Lab01 added file Primes.java

--- src/lab01/Primes.java
+++ src/lab01/Primes.java
@@ -1,7 +1,22 @@
 package lab01;
 
+//класс для поиска простых чисел в диапазоне от 2 до 100
 public class Primes {
     public static void main(String[] args){
-        
+        for (int i = 2; i <= 100; i++){
+            if (isPrime(i) == true){
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    //метод для проверки является ли число простым
+    public static boolean isPrime(int n){
+        for (int i = 2; i < n; i++){
+            if (n % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
